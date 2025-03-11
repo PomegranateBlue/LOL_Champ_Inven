a5decb337249423748daf5d41516f7ed8e51ceab
bluepome98 <wpdn1998@gmail.com>
2025-03-11 12:28:55 +0900

    네비게이션 및 파일구조 설정 중

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function Navigation() {
+  return (
+    <nav className="flex  p-4 bg-gray-800 text-white">
+      <ul className="flex">
+        <li>
+          <Link href="/">홈</Link>
+        </li>
+        <li>
+          <Link href="/">챔피언 목록</Link>
+        </li>
+        <li>
+          <Link href="/">아이템 목록</Link>
+        </li>
+        <li>
+          <Link href="/">챔피언 로테이션</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
