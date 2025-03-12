01ce237342af24a3bec27f1dbdddc44a3a42888b
bluepome98 <wpdn1998@gmail.com>
2025-03-12 19:43:56 +0900

    아이템 목록 구성

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -5,11 +5,11 @@ import { ItemListInterface } from "@/types/itemType";
 
 export default async function ItemPage() {
   const itemData: ItemListInterface[] = await fetchItemsListData();
-  console.log(itemData);
+
   return (
-    <div>
-      <h2 className="text-red-500 text-3xl">아이템 목록</h2>
-      <div className="grid grid-cols-6 gap-10 p-6">
+    <div className="p-6">
+      <h2 className="text-red-500 text-3xl p-2">아이템 목록</h2>
+      <div className="grid grid-cols-6 gap-6 ">
         {itemData.map((item) => (
           <Link key={item.name} href={`/items/${item.name}`}>
             <ItemCard
--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -7,15 +7,16 @@ export default function ItemCard({
   plaintext,
 }: ItemListInterface) {
   return (
-    <div>
+    <div className="border-white border-[1px] rounded-md flex flex-col w-[220px] h-[220px] items-center justify-center ">
       <Image
         src={`https://ddragon.leagueoflegends.com/cdn/15.5.1/img/item/${image.full}`}
-        width={120}
-        height={120}
+        width={80}
+        height={80}
         alt={name}
+        className="mx-auto p-2"
       ></Image>
-      <div>{name}</div>
-      <div>{plaintext}</div>
+      <div className="text-red-500 text-xl font-bold p-2">{name}</div>
+      <div className="text-gray-400 p-2">{plaintext}</div>
     </div>
   );
 }
