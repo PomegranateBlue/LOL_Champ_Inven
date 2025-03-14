d8e7968a75328ca656c1dc211532f6463f730431
bluepome98 <wpdn1998@gmail.com>
2025-03-14 12:41:21 +0900

    현재 챔피언 디테일페이지 정상작동

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -6,8 +6,8 @@ export default async function ChampionPage() {
   const championData: Champion[] = await fetchChampionListData();
   return (
     <div>
-      <h2 className="font-bold text-red-500 text-3xl p-4">챔피언 목록</h2>
-      <div className="grid grid-cols-4 gap-14 p-6">
+      <h2 className="font-bold text-red-500 text-3xl p-2">챔피언 목록</h2>
+      <div className="grid grid-cols-[repeat(auto-fit,_minmax(150px,_1fr))] items-center justify-start gap-4 p-8">
         {championData.map((champion) => {
           return <ChampionCard key={champion.id} champion={champion} />;
         })}
--- src/components/ChampionCard.tsx
+++ src/components/ChampionCard.tsx
@@ -7,16 +7,22 @@ type ChampionCardProps = {
 };
 export default function ChampionCard({ champion }: ChampionCardProps) {
   return (
-    <div key={champion.key}>
+    <div
+      key={champion.key}
+      className="relative flex flex-col items-center p-4 bg-gray-900 border border-gray-700 rounded-lg shadow-lg hover:scale-105 hover:bg-gray-800 transition-transform duration-300 sm:w-40 md:w-48"
+    >
       <Link href={`/champions/${champion.id}`}>
         <Image
           src={`${RIOT_URL.CHAMPION_IMAGE}/${champion.image.full}`}
           alt={champion.name}
           width={120}
           height={120}
+          className="border-[2px] rounded-md border-white-200"
         ></Image>
-        <div className="text-2xl text-red-500 font-bold">{champion.name}</div>
-        <div>{champion.title}</div>
+        <div className="mt-2 text-xl font-bold text-red-400">
+          {champion.name}
+        </div>
+        <div className="text-sm text-gray-400">{champion.title}</div>
       </Link>
     </div>
   );
