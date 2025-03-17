00bfbda5d9b992ae1a2b4e4a3465102002e7ea47
bluepome98 <wpdn1998@gmail.com>
2025-03-17 16:45:19 +0900

    global-error 설정 후 테스트 완료

--- src/app/items/[id]/page.tsx
+++ src/app/items/[id]/page.tsx
@@ -16,17 +16,23 @@ export default async function ItemDetailPage({ params }: Props) {
   return (
     <div>
       {item && (
-        <>
+        <div className="flex flex-col items-center p-8">
           <Image
+            className="border-white border-[2px] rounded-md"
             priority
             src={`https://ddragon.leagueoflegends.com/cdn/15.5.1/img/item/${item[0]}.png`}
             alt="item illustation"
             width={150}
             height={150}
           />
-          <h1>{item[1].name}</h1>
-          <p>{item[1].plaintext}</p>
-        </>
+          <h1 className="text-2xl font-bold m-2">{item[1].name}</h1>
+          <p className="font-semibold m-2">{item[1].plaintext}</p>
+          <div className="flex flex-col gap-2">
+            <p>재료값 : {item[1].gold.base}</p>
+            <p>완성비용 : {item[1].gold.total}</p>
+            <p>판매가격 : {item[1].gold.sell}</p>
+          </div>
+        </div>
       )}
     </div>
   );
